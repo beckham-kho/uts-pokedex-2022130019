8ca8ca74069f5a3f9236bc23a2da9935804cb3d9
beckham-kho <beckham44997@gmail.com>
2024-10-13 21:37:52 +0700

    feat(auth): add user auth

---
+++ database/migrations/2024_10_13_043331_create_pokemon_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pokemon', function (Blueprint $table) {
+            $table->string('name', 255);
+            $table->string('species', 100);
+            $table->string('primary_type', 50);
+            $table->decimal('weight', 8, 2)->default(0);
+            $table->decimal('height', 8, 2)->default(0);
+            $table->integer('hp');
+            $table->integer('attack');
+            $table->integer('defense');
+            $table->boolean('is_legendary')->default(false);
+            $table->string('photo')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pokemon');
+    }
+};
